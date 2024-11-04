75abf24e0e82f41aec7b552da4311f53298b2dec
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2024-11-04 21:16:20 +0100

    update

--- main.cpp
+++ main.cpp
@@ -13,19 +13,18 @@ int main(const int argc, const char **argv) {
         return 42;
     }
     
-    // Schritt 1: Daten aus der Datei lesen
+    // Schritt 1: Auslesen
     FILE* input_file = fopen(argv[2], "rb");
     if (!input_file) {
         cerr << "Fehler beim Öffnen der Eingabedatei!" << endl;
         return 23;
     }
 
-    // Puffer zum Speichern der Daten
     vector<unsigned char> buffer;
     unsigned char temp[4096]; // Temporärer Puffer
     size_t bytes_read;
 
-    // Lese die Datei in den Puffer
+    // Buffer beschreiben
     while ((bytes_read = fread(temp, sizeof(unsigned char), sizeof(temp), input_file)) > 0) {
         buffer.insert(buffer.end(), temp, temp + bytes_read);
     }
@@ -48,7 +47,7 @@ int main(const int argc, const char **argv) {
         return 11;
     }
 
-    // Schritt 3: In die neue Datei schreiben
+    // Schritt 3: Ausgabe schreiben
     FILE* output_file = fopen(argv[3], "wb");
     if (!output_file) {
         cerr << "Fehler beim Öffnen der Ausgabedatei!" << endl;
