729ceca608430375bf39d2db72faa4c4ec5427f3
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2024-11-18 06:49:25 +0100

    update

--- src/System/In.cpp
+++ src/System/In.cpp
@@ -5,17 +5,14 @@
 #include "In.h"
 
 #include <iostream>
-#include <set>
 
 #include "Out.h"
 #include "System.h"
 
 void In::readLine(string &target, const string &text) {
-    System system;
-
     Out::print(text);
     cout << Out::setColor(FG_LIGHT_CYAN);
     getline(cin, target);
     cout << Out::setColor(FG_WHITE);
-    if(system.hasToken(target)) System::exit();
+    if(System system; system.hasToken(target)) System::exit();
 }
